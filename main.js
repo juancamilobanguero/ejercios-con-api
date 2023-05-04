4627800b5418a734d017a879b07aa69a519e595c
juancamilobanguero <banguerojuancamilo58@gmail.com>
2023-05-04 09:51:26 -0500

    peliculas122

--- main.js
+++ main.js
@@ -1,1 +1,30 @@
-const peliculascontainer = document.querySelector(`.peliculas-container`)
+const cargarPeliculas = async() => {
+    try {
+        const respuesta = await fetch(`https://api.themoviedb.org/3/movie/popular?api_key=192e0b9821564f26f52949758ea3c473&language=es-MX&`)
+
+        console.log(respuesta);
+
+        // Si la respuesta es correcta
+        if(respuesta.status === 200){
+            const datos = await respuesta.json()
+
+            let peliculas = ''
+            datos.results.forEach(pelicula => {
+                peliculas += 
+                    <div class="pelicula">
+                        <img class="imagen" src="https://image.tmdb.org/t/p/w500/${pelicula.poster_path}"></img>
+                        <h3 class="titulo">${pelicula.title}</h3>
+                    </div>
+                
+            });
+
+            document.getElementById('contenedor').innerHTML = peliculas
+        } 
+
+    } catch(error){
+        console.log(error)
+    }
+
+ }
+
+cargarPeliculas()
